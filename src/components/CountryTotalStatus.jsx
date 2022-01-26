61deeebffb3e9b4f1187f438319b5ed3a61e6b04
savbeM <mark.selihov2458@gmail.com>
2022-01-26 17:25:58 +0300

    sort handle change

--- src/components/CountryTotalStatus.jsx
+++ src/components/CountryTotalStatus.jsx
@@ -5,39 +5,69 @@ import {Spinner} from "react-bootstrap";
 
 
 export const CountryTotalStatus = (props) => {
-    return (<div className="countryStatusTable">
+
+    const [sortByConfirmed, setSortByConfirmed] = useState(true)
+    const [sortByDeaths, setSortByDeaths] = useState(false)
+    const [sortByAlphabet, setSortByAlphabet] = useState(false)
+    const [sortedInformation, setSortedInformation] = useState(props.countryData)
+
+    const sortBy = (data) => {
+        switch (true) {
+            case sortByConfirmed:
+                return data.sort((a,b) => b.latest_data.confirmed - a.latest_data.confirmed)
+            case sortByDeaths:
+                return data.sort((a,b) => b.latest_data.deaths - a.latest_data.deaths)
+            case sortByAlphabet:
+                return data.sort((a,b) => b.code - a.code)
+        }
+    }
+
+    const sortByAlphabetHandle = () => {
+        setSortByAlphabet(true)
+        setSortByConfirmed(false)
+        setSortByDeaths(false)
+    }
+
+    const sortByDeathsHandle = () => {
+        setSortByAlphabet(false)
+        setSortByConfirmed(false)
+        setSortByDeaths(true)
+    }
+
+    const sortByConfirmedHandle = () => {
+        setSortByAlphabet(false)
+        setSortByConfirmed(true)
+        setSortByDeaths(false)
+    }
+
+
+    useEffect(() => { setSortedInformation(sortBy(props.countryData))}, [props.countryData])
+
+
+
+    return (<div className="countryStatusTable">k
             <Table striped bordered hover variant="dark">
                 <thead>
                 <tr>
-                    <th>Country</th>
-                    <th>Confirmed</th>
-                    <th>Deaths</th>
+                    <th onClick={() => sortByAlphabetHandle()}>Country</th>
+                    <th onClick={() => sortByConfirmedHandle()}>Confirmed</th>
+                    <th onClick={() => sortByDeathsHandle()}>Deaths</th>
                 </tr>
                 </thead>
-                <CountryStatusTableContent countryData={props.countryData}/>
+                <CountryStatusTableContent setMoreAboutCountry={props.setMoreAboutCountry} moreAboutCountry={props.moreAboutCountry} sortedInformation={sortedInformation}  />
             </Table>
         </div>
     )
 }
 
 export const CountryStatusTableContent = (props) => {
-    const [sortByConfirmed, setSortByConfirmed] = useState(true)
-    const [sortedInformation, setSortedInformation] = useState(props.countryData)
-    const sortBy = (data) => {
-        if (sortByConfirmed === true) {
-           return data.sort((a,b) => b.latest_data.confirmed - a.latest_data.confirmed)
-        }
-    }
-
-    useEffect(() => { setSortedInformation(sortBy(props.countryData))}, [props.countryData])
-
 
     return (<>{
-            sortedInformation ?  sortedInformation.map(i => {
+            props.sortedInformation ?  props.sortedInformation.map(i => {
                 return (
                     <tbody key={i.code}>
                     <tr>
-                        <td>{i.name}</td>
+                        <td onClick={() => props.setMoreAboutCountry({value: true, id: i.code})}>{i.name}</td>
                         <td>{i.latest_data.confirmed}</td>
                         <td>{i.latest_data.deaths}</td>
                     </tr>
