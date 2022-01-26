cb559735fb40ff5f0f3d34f85c3d8dd90d98a466
savbeM <mark.selihov2458@gmail.com>
2022-01-26 17:26:04 +0300

    sort handle change

--- src/components/CovidStatusMain.jsx
+++ src/components/CovidStatusMain.jsx
@@ -4,29 +4,35 @@ import {Row, Spinner} from "react-bootstrap";
 import {CountryTotalStatus} from "./CountryTotalStatus";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
+import {MoreAboutCountry} from "./MoreAboutCountry";
 
 export const CovidStatusMain = () => {
+    const [moreAboutCountry, setMoreAboutCountry] = useState({value:false, id: null})
     const [countryData, setCountryData] = useState()
-    useEffect (() => {
-       axios.get("https://corona-api.com/countries").then(
-           data =>  setCountryData(data.data.data)
-       )
-    }, [setTimeout(10000)])
-
+    useEffect(() => {
+            axios.get("https://corona-api.com/countries").then(
+                data => setCountryData(data.data.data)
+            )
+        }
+    )
 
     if (countryData !== undefined) {
-        return(
-            <div className="">
+        return (
+            <div className="appWrapper">
                 <Container>
                     <Row>
-                        <Col><CountryTotalStatus countryData={countryData}/></Col>
-                        <Col>2 of 2</Col>
+                        <Col>
+                            <h1 className="articleTitle">TOTAL CASES</h1>
+                            <CountryTotalStatus setMoreAboutCountry={setMoreAboutCountry} moreAboutCountry={moreAboutCountry} countryData={countryData}/>
+                        </Col>
+                        <Col>{moreAboutCountry.value ? <MoreAboutCountry moreAboutCountry={moreAboutCountry} counrtyData={countryData}/> : null}</Col>
                     </Row>
                 </Container>
             </div>
         )
+    } else {
+        return <Spinner animation="grow"/>
     }
-    else { return <Spinner animation="grow" />}
 
 }
 
